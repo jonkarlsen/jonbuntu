7ed940b73c05f31ac7f084e7ade2ebe81644fcab
Jon Karlsen <karlsen.jon.a@gmail.com>
2025-12-16 21:56:17 +0100

    remove roger

--- fastapi_service/src/espen.py
+++ fastapi_service/src/espen.py
@@ -1,5 +1,5 @@
-import re
 import os
+import re
 from datetime import datetime
 from pathlib import Path
 from zoneinfo import ZoneInfo
@@ -9,14 +9,12 @@ from fastapi.responses import FileResponse
 
 espen_route = FastAPI()
 
-# VIDEO_PATH = Path(__file__).parent / "static/videos/espen.mp4"
-# BASE_PATH = Path(__file__).parent / "static/videos"
+
 if os.environ.get("IN_DOCKER") == "yes":
     BASE_PATH = Path("/app/static/videos")
 else:
     BASE_PATH = Path(__file__).parent / "static" / "videos"
 BASE_PATH.mkdir(parents=True, exist_ok=True)
-VIDEO_PATH_ROGER = Path(__file__).parent / "static/videos/roger.mp4"
 OSLO_TZ = ZoneInfo("Europe/Oslo")
 
 
@@ -35,10 +33,3 @@ async def get_video():
     idx = (days_since_epoch + -1) % len(video_files)
     video_path = video_files[idx]
     return FileResponse(video_path, media_type="video/mp4")
-
-
-@espen_route.get("/roger")
-async def get_roger():
-    if VIDEO_PATH_ROGER.exists():
-        return FileResponse(VIDEO_PATH_ROGER, media_type="video/mp4")
-    return {"error": "Video not found"}
